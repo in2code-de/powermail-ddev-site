29cfa16f77bde2fbd0adfd17831d238f24f570a9
Marcus Schwemer <marcus.schwemer@in2code.de>
2025-11-06 13:47:31 +0100

    [FEATURE] Add introduction package for EXT:powermail_cond

---
+++ packages/introduction_powermail_cond/ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Powermail development site :: Powermail_cond ',
+    'description' => 'Powermail development site : Powermail_cond',
+    'category' => 'distribution',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '*',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'in2code GmbH',
+    'author_email' => 'service@dkd.de',
+    'author_company' => 'in2code',
+    'version' => '1.0.0',
+];
