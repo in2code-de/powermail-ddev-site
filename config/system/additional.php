551e41943ba9f76149a649b7a8c34889f9900142
Marcus Schwemer <marcus.schwemer@in2code.de>
2025-11-06 15:12:01 +0100

    [CLEANUP] Remove unused lines

--- config/system/additional.php
+++ config/system/additional.php
@@ -6,14 +6,12 @@ $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
         'MAIL' => [
             'defaultMailFromAddress' => 'docker@localhost',
             'defaultMailFromName' => 'Powermail DDEV Site',
-            // 'transport' => 'sendmail',
         ],
 
         'SYS' => [
             'errorHandlerErrors' => E_WARNING | E_USER_ERROR | E_USER_WARNING | E_USER_NOTICE | E_RECOVERABLE_ERROR | E_DEPRECATED | E_USER_DEPRECATED,
             'exceptionalErrors' => E_WARNING | E_RECOVERABLE_ERROR | E_DEPRECATED | E_USER_DEPRECATED,
             'trustedHostsPattern' => 'localhost|powermail-ddev-site.ddev.site',
-
         ]
     ]
 );
